22a42f9adba63e3f3ce820ac3cb2c24a05c065e2
eddie jimenez <ejmrtears89@gmail.com>
2019-08-16 18:32:51 -0700

    commiting work for object-arrays section

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,27 +8,49 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const tyrannosaurus = {
+  name: "Tyrannosaurus",
+  diet: "Carnivorous",
+  weight: 7000 + "kg",
+  length: 12 + "m",
+  period: "Late Cretaceous",
+  roar: function() {
+    return "RAWERSRARARWERSARARARRRR!"
+  }
+} 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const stegosaurus = {
+  name: "Stegosaurus",
+  diet: "Herbivorous",
+  weight: 2000 + "kg", 
+  length: 9 + "m",
+  period: "Late Jurassic"
+} 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const velociraptor = {
+  name: "Velociraptor",
+  diet: "Carnivorous",
+  weight: 15 + "kg", 
+  length: 1.8 + "m",
+  period: "Late Cretaceous"
+} 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -50,6 +72,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+graduates.forEach(function(value){
+  universities.push(value.university);
+  return universities.sort();
+})
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -59,6 +85,10 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(function(value){
+  contactInfo.push(`${value.first_name} ${value.email}`);
+  return contactInfo
+})
 console.log(contactInfo);
 
 
